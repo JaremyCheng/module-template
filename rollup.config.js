ddc219b58b3bff0d65f1367561333a203d40c44c
JaremyCheng <xingfu7yi@hotmail.com>
2020-03-20 11:26:31 +0800

    build(rollup): rollup配置更新,调整package.json文件

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,35 @@
+import path from 'path';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 const production = !process.env.ROLLUP_WATCH;
+const sourcemap = !!process.env.sourcemap;
+const compress = !!process.env.compress;
+const resolvePath = (p) => path.resolve(__dirname, p);
+const pkg = require(resolvePath(`package.json`));
 
 export default {
   input: 'src/index.ts',
-  output: {
-    sourcemap: !production,
-    file: 'dist/index.js',
-    format: 'esm'
-  },
+  output: [
+    {
+      sourcemap: sourcemap,
+      file: pkg.module,
+      format: 'esm'
+    },
+    {
+      sourcemap: sourcemap,
+      file: pkg.main,
+      format: 'cjs'
+    }
+  ],
   cache: !production,
   plugins: [
+    commonjs(),
+    resolve(),
     typescript(),
     // 压缩
-    production && terser()
+    compress && terser()
   ]
 };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-import { isEmpty, forIn, cloneDeep } from 'lodash-es';
+import { isEmpty, forIn, cloneDeep, clone } from 'lodash-es';
 
-export { isEmpty, forIn, cloneDeep };
+export function exoCloneDeep(obj: object) {
+  return cloneDeep(obj);
+}
+// export function getName(name:string):void {
+//   console.log(name);
+// }
+
+// export function getLife(num:number):void {
+//   console.log(num);
+// }
